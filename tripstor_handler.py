55daedaa90be9e49fff535d68a0cc084242bb0cf
deadfrogface <dead0frog0face@gmail.com>
2025-07-02 12:23:19 +0200

    Delete tripstor_handler.py

--- tripstor_handler.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def generate_3d_from_image(image_path, outpath="exports/triposr_output.obj"):
-    print("TripoSR läuft auf", image_path)
-    subprocess.run(["echo", "TriposR stubing..."])
-    shutil.copy(image_path, outpath)
-    return outpath

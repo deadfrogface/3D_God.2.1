a9fa9cc4d6084e851de0c0058ceaeb63ee818e2a
deadfrogface <dead0frog0face@gmail.com>
2025-07-02 11:46:52 +0200

    Aktualisieren von main_launcher.py

--- main_launcher.py
+++ main_launcher.py
@@ -1,1 +1,17 @@
-.gitinore
+import sys
+from PySide6.QtWidgets import QApplication
+from ui.gui_main_window import MainWindow
+import json
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    try:
+        with open("config.json", "r") as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        config = {"theme": "dark", "nsfw_mode": True, "controller_enabled": True}
+
+    window = MainWindow(config)
+    window.show()
+    sys.exit(app.exec())
